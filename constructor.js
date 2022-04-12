9c1128a62ca91248168a230d614a2e7ae7f13fd2
Sunny9899 <63901097+Sunny9899@users.noreply.github.com>
2022-04-12 18:31:16 +0530

    Commit

--- constructor.js
+++ constructor.js
@@ -1,1 +1,13 @@
 //constructor using new
+
+function Player(n,t){
+    this.name=n;
+    this.team=t;
+}
+
+var player1=new Player("Virat","India");
+var player2= new Player("Rohit","Sharma");
+
+
+console.log(player1,player2);
+console.log(player1.name,player2.name); 
